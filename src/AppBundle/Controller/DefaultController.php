108d094445f4ead20bb01abb98568c82ff10fefb
PiotrekKustra <piotrek.kustra@hotmail.com>
2015-03-05 13:50:57 +0100

    Mury

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -76,7 +76,7 @@ class DefaultController extends Controller
     /*
      * M
      */
-/**
+	/**
      * @Route("/piosenka-mury.html", name="piosenka-mury")
      */
     public function piosenkaMuryAction()
