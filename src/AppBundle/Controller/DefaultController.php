1d06c3f8e9aaa12e316db5cbcc869c28fd5f619c
PiotrekKustra <piotrek.kustra@hotmail.com>
2015-03-15 20:02:25 +0100

    Dodanie piosenki - Miasto budzi sie

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -203,6 +203,13 @@ public function korowodyAction()
     {
         return $this->render('default/piosenka-mury.html.twig');
     }
+    /**
+     * @Route("/piosenka-miasto-budzi-sie.html", name="piosenka-miasto-budzi-sie")
+     */
+    public function piosenkaMiastoBudziSieAction()
+    {
+        return $this->render('default/piosenka-miasto-budzi-sie.html.twig');
+    }
 
     /*
      * N
